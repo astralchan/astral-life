1fd3e4792b713f2e62cfc551c766e9c733d7c1b3
amber <astral@astralchan.xyz>
2025-04-29 12:50:46 -0600

    Fix build script for BSD / Mac
    On BSD / mac, use sysctl instead of nproc

--- build.sh
+++ build.sh
@@ -23,7 +23,18 @@ if [ "$TOOLCHAIN_FILE" ]; then
   CMAKE_CMD="$CMAKE_CMD -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE"
 fi
 
+CMAKE_BUILD_CMD="cmake --build build --config $BUILD_TYPE"
+
+case "$(uname)" in
+  Darwin | *BSD)
+    CMAKE_BUILD_CMD="$CMAKE_BUILD_CMD --parallel $(sysctl -n hw.ncpu)"
+    ;;
+  Linux)
+    CMAKE_BUILD_CMD="$CMAKE_BUILD_CMD --parallel $(nproc --all)"
+    ;;
+esac
+
 $CMAKE_CMD
-cmake --build build --config "$BUILD_TYPE" --parallel "$(nproc --all)"
+$CMAKE_BUILD_CMD
 
 exit 0
